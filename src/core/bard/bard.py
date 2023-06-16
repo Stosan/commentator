66a437a686cd5b9076db0f08062f9496a9e42a3a
Ayo Tosan Samuel <46663234+Stosan@users.noreply.github.com>
2023-06-16 23:51:27 +0100

    updated language detector

--- src/core/bard/bard.py
+++ src/core/bard/bard.py
@@ -3,12 +3,14 @@ from src.config import appconfig
 
 
 def RunBard(langtype: str, query: str):
+    print(query,langtype)
     try:
         PROMPT_KEY = f"PROMPT: Add inline comprehensive commenting to this {langtype} code. Provide a detailed explanation of how the function works as a comment at the top of the function. Avoid including examples of how to use the function, and refrain from adding any additional notes after the commented code."
         code = f'''{query}'''
         PROMPT=f"{PROMPT_KEY}{code}"
         bard = Bard(token=appconfig.bard_key, timeout=30)
         res = bard.get_answer(PROMPT)['content']
+        print(res)
         return res,None
     except Exception as e:
         return None,e
--- src/utils/languagechecker.py
+++ src/utils/languagechecker.py
@@ -1,12 +1,21 @@
+import re
+
 def detect_programming_language(function: str) -> str:
     # Convert the function to lowercase for case-insensitive comparison
     function = function.lower()
 
-    if function[:3] == "def":
-        return "Python"
-    elif function[:4] == "func":
-        return "Go"
-    elif function[:8] == "function":
+    if re.search(r"^async\s+function", function):
         return "JavaScript"
+    elif re.search(r"^function", function):
+        return "JavaScript"
+    elif re.search(r"^def", function):
+        return "Python"
+    elif re.search(r"^async\s+def", function):
+        return "Python"
+    elif re.search(r"^\s*go\s+func", function):
+        return "Go"
+    elif re.search(r"^func", function):
+        return "Go"
+    
 
     return "Unknown"
